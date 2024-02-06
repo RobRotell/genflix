d54d6e777ce057d6ad6fdc82e5a3f7fc7317d418
Rob <dev@robrotell.com>
2024-02-05 22:53:52 -0500

    Adding components, renaming pages

---
+++ src/components/Layout/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'normalize.css'
+import './style.module.css'
+
+
+const Layout = ({ children }) => (
+    <>
+		{/* <Header /> */}
+        {children}
+    	{/* <Footer /> */}
+    </>
+)
+
+
+export default Layout
