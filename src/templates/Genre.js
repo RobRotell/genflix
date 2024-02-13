937e2a647224c8637afabe725daefb944c34c087
Rob <dev@robrotell.com>
2024-02-12 22:43:57 -0500

    Working on individual genre pages

--- src/templates/Genre.js
+++ src/templates/Genre.js
@@ -1,42 +1,30 @@
 import { graphql } from 'gatsby'
 import React from 'react'
+import MovieGrid from '../components/MovieGrid'
+import FixedContainer from '../components/FixedContainer'
 
 
 export default function SingleGenreTemplate({ data, pageContext }) {
-	const genre = pageContext.genreName
-	let movies = data.allRestApiGetMovies.edges[0].node.movies // I know, I know
+	let { movies } = pageContext
 
-	movies = movies.filter( movie => genre === movie.genre )
+	movies = movies.sort( ( a, b ) => {
+		const aTitle = a.title.toLowerCase()
+		const bTitle = b.title.toLowerCase()
 
-	console.log( movies )
-
-
+		if( aTitle < bTitle ) {
+			return -1
+		} else if( bTitle < aTitle ) {
+			return 1
+		} else {
+			return 0
+		}
+	})
 
 	return (
-		<>
-			bob
-		</>
+		<FixedContainer>
+			<MovieGrid
+				movies={movies}
+			/>
+		</FixedContainer>
 	)
 }
-
-
-// todo -- fix query so that it only pulls movie of specific genre (conflicts with REST plugin?)
-export const query = graphql`
-	query {
-		allRestApiGetMovies {
-			edges {
-				node {
-					movies {
-						id
-						title
-						tagline
-						director
-						genre
-						year
-						imageUrl
-					}
-				}
-			}
-		}
-	}
-`
