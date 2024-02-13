474b4d3e03dd10f3d62c5336a88e3c6a7d4d7f5b
Rob <dev@robrotell.com>
2024-02-12 22:42:15 -0500

    Building out logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as styles from './style.module.css'
+
+
+const Logo = ({ width = 180, height = 80 }) => {
+
+	return (
+		<div className={styles.logoContainer}>
+			<>Logo</>
+		</div>
+	)
+}
+
+
+export default Logo
