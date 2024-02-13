0809cd7f36f7a0aaa89ec10cca9c3540e9b9e2cf
Rob <dev@robrotell.com>
2024-02-12 22:44:32 -0500

    Helper function for ucword-ing strings

---
+++ src/utils/ucWordsString.js
@@ -0,0 +1,15 @@
+/**
+ * Capitalize the first letter in each word in a string
+ *
+ * @param {string} string
+ * @return {string}
+ */
+export const ucWordString = string => {
+	const words = string.split( ' ' )
+
+	words.forEach( ( word, i ) => {
+		words[i] = word.charAt(0).toUpperCase() + word.slice(1)
+	})
+
+	return words.join( ' ' )
+}
