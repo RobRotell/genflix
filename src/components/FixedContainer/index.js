5a146ff125d5a3b890dad74417536062dd8eaa5d
Rob <dev@robrotell.com>
2024-02-12 22:44:08 -0500

    Component for fixed width

---
+++ src/components/FixedContainer/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as styles from './style.module.css'
+
+
+const FixedContainer = ({ children }) => {
+
+
+	return (
+		<div className={styles.container}>
+			{children}
+		</div>
+	)
+
+}
+
+
+export default FixedContainer
