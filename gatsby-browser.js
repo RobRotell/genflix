d2bbc7dd79c7ae72fd301b661e42a69c4439fe8d
Rob <dev@robrotell.com>
2024-02-21 21:20:09 -0500

    Moving normalize

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from './src/components/Layout'
 
+import 'normalize.css'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/700.css'
